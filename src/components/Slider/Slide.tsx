e0f1dd8a1d61f6bbf67678b2920c5145eabdea6b
Kamil Adamczyk <mybach@gmail.com>
2024-03-27 21:51:09 +0100

    Added fading effect when slide changes

--- src/components/Slider/Slide.tsx
+++ src/components/Slider/Slide.tsx
@@ -1,16 +1,44 @@
+import { useEffect, useRef, useState } from 'react'
+
 type SlideProps = {
   imageSrc: string
   children?: string | JSX.Element
 }
 
 export function Slide({ imageSrc, children }: SlideProps) {
+  const imageSrcRef = useRef<ReturnType<typeof String> | null>(null)
+  const timeoutRef = useRef<ReturnType<typeof window.setTimeout> | null>(null)
+  const [isApplied, setIsApplied] = useState(true)
+
+  useEffect(() => {
+    if (timeoutRef.current) {
+      clearTimeout(timeoutRef.current)
+    }
+
+    if (imageSrcRef.current !== imageSrc) {
+      console.log(
+        'imageSrcRef.current !== imageSrc',
+        imageSrcRef.current !== imageSrc,
+      )
+      imageSrcRef.current = imageSrc
+      setIsApplied(true)
+    }
+
+    timeoutRef.current = setTimeout(() => {
+      setIsApplied(false)
+    }, 300)
+  }, [imageSrc])
+
   return (
     <div
-      key={imageSrc}
       style={{ backgroundImage: `url(${imageSrc})` }}
       className="w-full h-full bg-cover bg-center text-white grow-0 shrink-0"
     >
-      {children}
+      <div
+        className={`w-full h-full ${isApplied ? 'animate-fade' : ''} ${children ? 'bg-gradient-to-r from-slate-900/[.9] from-30% to-transparent p-20' : ''}`}
+      >
+        {children}
+      </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,17 @@
 export default {
   content: ['./index.html', './src/**/*.{js,jsx,ts,tsx}'],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        fade: 'fadeIn .3s ease-in-out',
+      },
+      keyframes: theme => ({
+        fadeIn: {
+          '0%': { backgroundColor: theme('colors.white') },
+          '100%': { backgroundColor: theme('colors.transparent') },
+        },
+      }),
+    },
   },
   plugins: [],
 }
